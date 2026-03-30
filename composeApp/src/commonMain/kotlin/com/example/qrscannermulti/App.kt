d3c9e0e4ec32e941a4ebb9b043e1c3a64856999b
varlamovartem05-star <varlamovartem05@gmail.com>
2026-03-30 12:29:31 +0900

    Update App.kt

--- composeApp/src/commonMain/kotlin/com/example/qrscannermulti/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/qrscannermulti/App.kt
@@ -1,5 +1,8 @@
 package com.example.qrscannermulti // УБЕДИТЕСЬ, ЧТО ЭТО ВАШ ПАКЕТ
 
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
